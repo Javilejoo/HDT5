41c8594f331ecdcec63f8e53349ee4a42c7c4a05
Javilejoo <pra21486@uvg.edu.gt>
2022-03-12 22:46:00 -0600

    prueba de waiting y ready

--- SIMULACION.py
+++ SIMULACION.py
@@ -16,13 +16,16 @@ def proceso(nombre, env, memoria, cpu, llegada, cantidad_instrucciones, cantidad
     #grabo el tiempo de llegada
     tiempo_llegada = env.now
 
+    #PROCESO NEW
     print('%s proceso en cola [NEW llegada] -> %d cantidad ram requerida %d, disponible %d' % (nombre, env.now, cantidad_ram, memoria.level))
     yield memoria.get(cantidad_ram)  #Pide la memoria que necesita o espera automaticamente hasta que haya suficiente
 
+    #PROCESO READY
     while cantidad_instrucciones > 0:  #repite hasta que se acabe la cantidad de instrucciones pendientes
         # Ya tiene memoria para iniciar
         print('%s proceso en cola READY tiempo -> %d cantidad instrucciones pendientes %d' % (nombre, env.now, cantidad_instrucciones))
 
+        #PROCESO RUNNING
         with cpu.request() as req:  #pide el procesador
             yield req
 
@@ -31,21 +34,32 @@ def proceso(nombre, env, memoria, cpu, llegada, cantidad_instrucciones, cantidad
 
             # Ya tiene procesador
             print('%s proceso en estado RUNNING fue atendido en tiempo -> %d cantidad ram %d, Instrucciones pendientes %d ram disponible %d' % (nombre, env.now, cantidad_ram, cantidad_instrucciones, memoria.level))
+            if  cantidad_instrucciones >= 0:
+                numeroRandom = random.randint(1, 2)
+                if numeroRandom == 1:
+                    yield env.timeout(1)
+                    print('%s proceso en estado WAITING(haciendo operaciones de I/O..) fue atendido en tiempo -> %d cantidad ram %d, Instrucciones pendientes %d ram disponible %d' % (nombre, env.now, cantidad_ram, cantidad_instrucciones, memoria.level))
+                else:
+                    pass
+            else:
+                # Cuando ya finaliza devuelve la memoria utilizada
+                yield memoria.put(cantidad_ram)
 
-    # Cuando ya finaliza devuelve la memoria utilizada
-    yield memoria.put(cantidad_ram)
+                #PROCESO TERMINATED
+                print('%s proceso TERMINATED salida -> %d cantidad ram devuelta %d, nueva cantidad de memoria disponible %d' % (nombre, env.now, cantidad_ram, memoria.level))
+                global tiempo_total
+                tiempo_total += env.now - tiempo_llegada
+                print('Tiempo total %d' % (env.now - tiempo_llegada))
+                
 
-    print('%s proceso TERMINATED salida -> %d cantidad ram devuelta %d, nueva cantidad de memoria disponible %d' % (nombre, env.now, cantidad_ram, memoria.level))
-    global tiempo_total
-    tiempo_total += env.now - tiempo_llegada
-    print('Tiempo total %d' % (env.now - tiempo_llegada))
+    
 
 
 random.seed(10)
 env = simpy.Environment()  # crear ambiente de simulacion
 initial_ram = simpy.Container(env, 30, init=30)  # crea el container de la ram
 initial_cpu = simpy.Resource(env, capacity=1)  # se crea el procesador con capacidad establecida
-initial_procesos = 50  # cantidad de procesos a generar
+initial_procesos = 2 # cantidad de procesos a generar
 tiempo_total = 0
 
 for i in range(initial_procesos):
